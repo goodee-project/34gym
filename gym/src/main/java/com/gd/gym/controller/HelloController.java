c8287af3ea64c5249871a95c2eea60a62909c732
cucud <cucud@LAPTOP-9EG5VFTC>
2021-07-07 15:23:37 +0900

    [홍민성] 디버깅 메시지 메서드 구현
     - debug 클래스 구현
     - 함수를 불러와서 메서드 이름(위치를 알기 위해), 확인하고 싶은 변수명, 변수를 넣어주면 된다.
       (ex, debugging("getLoginAdmin", "admin", admin.toString()))
     - 메서드 이름, 변수
       (ex, de.debugging("getLoginAdmin", "DB 연동 로그인 테스트"))

--- gym/src/main/java/com/gd/gym/controller/HelloController.java
+++ gym/src/main/java/com/gd/gym/controller/HelloController.java
@@ -8,16 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.gd.gym.debug.debug;
 import com.gd.gym.service.AdminService;
 import com.gd.gym.vo.Admin;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Controller
 public class HelloController {
 	@Autowired AdminService AdminService;
 	
+	debug de = new debug();
 	
 	@GetMapping("/hello")
 	public String hello() {
@@ -27,8 +26,12 @@ public class HelloController {
 	@PostMapping("getLoginAdmin")
 	public String getLoginAdmin(Model model, HttpSession session, Admin admin) {
 		
+		
+		de.debugging("getLoginAdmin", "admin", admin.toString());
 		Admin loginAdmin = AdminService.getLoginAdmin(admin);
-		log.debug(loginAdmin+"DB 연동 로그인 테스트");
+		de.debugging("getLoginAdmin", "DB 연동 로그인 테스트");
+		de.debugging("getLoginAdmin", 123455);
+		//log.debug(loginAdmin+"DB 연동 로그인 테스트");
 		if(loginAdmin != null) {	// 로그인성공시 세션에 정보담기
 			session.setAttribute("loginAdmin", loginAdmin);
 		}
