62ae92f942009f7ab67b709225527e743921ac31
seoh2k <seoh2k@gmail.com>
2021-07-12 16:49:01 +0900

    [김서희] 강의실 CRUD 구현 중(강의실 목록 완료)
     - 강의실 목록(LectureRoomList) 완료
     - 강의실 입력, 수정, 삭제 mapper, service, controller 구현 완료
     - vo 작성 안함 (map 이용)

---
+++ gym/src/main/java/com/gd/gym/vo/Branch.java
@@ -0,0 +1,11 @@
+package com.gd.gym.vo;
+
+import lombok.Data;
+
+@Data
+public class Branch {
+	private int permissionId;
+	private int adminId;
+	private String branchName;
+	private String lastUpdate;
+}
