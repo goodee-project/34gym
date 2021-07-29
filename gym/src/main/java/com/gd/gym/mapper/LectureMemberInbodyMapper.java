25c548d7925310420b4c46ec6933949bf8aa88cd
seoh2k <seoh2k@gmail.com>
2021-07-29 10:21:50 +0900

    [김서희] 강좌회원 인바디 입력/목록조회 기능 구현
     - 운동이용권 회원 인바디 입력/목록조회 기능이랑 동일

---
+++ gym/src/main/java/com/gd/gym/mapper/LectureMemberInbodyMapper.java
@@ -0,0 +1,16 @@
+package com.gd.gym.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gd.gym.vo.LectureMemberInbody;
+
+@Mapper
+public interface LectureMemberInbodyMapper {
+	// 강좌 회원 인바디 입력
+	int insertLectureMemberInbody(LectureMemberInbody lectureMemberInbody);
+	// 강좌 회원 인바디 목록
+	List<Map<String, Object>> selectLectureMemberInbodyList(int memberId);
+}
